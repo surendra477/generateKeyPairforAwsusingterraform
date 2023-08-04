5b595056d31ff9da273f532b50a4e838469eaacf
surendra477 <surendraediga477@gmail.com>
2023-08-04 12:38:17 +0530

    final push

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f34c5ae932e6f0e4"
   instance_type = "t2.micro"
-  key_name = "demo"
+  key_name = "TF_key"
   tags = {
     Name = "terraformInstance"
   }
+}
+
+
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+resource "local_file" "TF-key" {
+    content  = tls_private_key.rsa.private_key_pem
+    filename = "tfkey"
 }
