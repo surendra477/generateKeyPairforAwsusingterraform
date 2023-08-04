5acfb521db826af7167d4d38704c80d92aeab5d4
surendra477 <surendraediga477@gmail.com>
2023-08-04 12:05:29 +0530

    key pair from aws

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_files= ["/Users/surendraediga/.aws/credentials"]
+  profile = "test"
+}
